26c099e3361d9d3b467e2199760b66d1ec9e26f3
spaceTab <93plaistj@protonmail.ch>
2019-04-29 16:08:57 -0400

    intake script

--- intake.sh
+++ intake.sh
@@ -3,5 +3,5 @@
 echo "Enter Dose Amount";
 read amount;
 
-echo "Time: $(date +'%m/%d %r') -- Dose: $amount)" >> ~/Documents/kratom-log/intake.log
+echo "Time: $(date +'%m/%d %r') -- Dose: $amount" >> ~/Documents/kratom-log/intake.log
 echo "LOGGED -> $(tail -1 ~/Documents/kratom-log/intake.log)"
