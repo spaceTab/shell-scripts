ce3c476330c58e7bbf91c9e7070280f33d7295fc
spaceTab <93plaistj@protonmail.ch>
2019-05-11 22:02:54 -0400

    rofi script

---
+++ rofiConf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# rofi -sidebar-mode \
+# -modi run,drun, window \
+# -lines 12 \
+# -padding 18 \
+# -width 60 \
+# -location 4 0 \
+# -show drun \
+# -columns 3 \
+# -font "Noto Sans 8"
+# -yoffset 50 
+
+rofi -modi drun,run 
+-show drun 
+-font "Fira Code Regular 13" 
+-show-icons 
+-icon-theme Qogir -location 0 
+-columns 2 
+-width 40
