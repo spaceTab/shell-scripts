34adc61adb4a4f94d4866768581148d80bfea48e
spaceTab <93plaistj@protonmail.ch>
2020-11-08 14:51:02 -0500

    ..

--- intake.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/env zsh
-
-echo "Enter Dose Amount";
-read amount;
-
-echo "Time: $(date +'%m/%d %r') -- Dose: $amount" >> ~/Documents/kratom-log/intake.log
-echo "LOGGED -> $(tail -1 ~/Documents/kratom-log/intake.log)"
