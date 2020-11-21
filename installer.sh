d9105c33f15bccc61ee035d2f9a5099277514a32
spaceTab <93plaistj@protonmail.ch>
2020-11-21 13:04:45 -0500

    copy paste error

--- installer.sh
+++ installer.sh
@@ -17,7 +17,7 @@ cd /run/media/pg/2B2F-4D27 && cp -r Music ~/Music &&
   cp -r theming ~/Downloads
 
 # Oh my ZSH install
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; 
 
 
 
