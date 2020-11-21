dd860a9e2debd5546812eded68968d95a67696c8
spaceTab <93plaistj@protonmail.ch>
2020-11-20 19:10:28 -0500

    installer sh fix

--- installer.sh
+++ installer.sh
@@ -3,18 +3,21 @@
 #manjaro installation script
 
 sudo pacman -Syyu
+pacman -S --noconfirm discord telegram yay fish
 
-# Create my normal directory structure
-cd ~/Documents
-mkdir Programming && cd Programming && mkdir Scripts
+#Create my directory structure
+mkdir -v Programming && cd Programming;
+git clone git@github.com:spaceTab/shell-scripts.git;
+git clone git@github.com:spaceTab/Bento.git;
 
+cd ~/Videos ; mkdir -v Media
 
-# Install software
-#OMZSH
+cd /run/media/pg/2B2F-4D27 && cp -r Music ~/Music && 
+  cp -r Wallpapers ~/Pictures &&
+  cp -r theming ~/Downloads
+
+# Oh my ZSH install
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; 
 
-pacman -S --noconfirm discord 
-pacman -S --noconfirm telegram
-pacman -S --noconfirm pcmanfm
 
-flatpak install flathub com.spotify.Client
+
