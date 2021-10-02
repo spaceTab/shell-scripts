c32c1e8f47976ed3d13d8663e9e829f15d836640
spaceTab <93plaistj@protonmail.ch>
2021-10-01 20:34:47 -0400

    ok

--- termstart.sh
+++ termstart.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 # All of these commands can befound pre-installed in ~/.bin (On arcolinux);
 
-
 array[0]="pfetch"
 array[1]="elfman"
 array[2]="bonsai-tree -L 17"
-array[3]="alpha"
-#array[4]="nerdfetch \n"
-#array[5]="owofetch"
-#array[6]="uwufetch"
-array[4]="sfetch"
-array[5]="afetch"
-array[6]="arcod"
-array[7]="colortest-slim"
-array[8]="ufetch"
+array[3]="sfetch"
+array[4]="colortest-slim"
+array[5]="ufetch"
+array[6]="pokemon-colorscripts -n togepi"
+array[7]="cat /home/j/booty.txt"
+array[8]="pokemon-colorscripts -n dwebble"
 
 size=${#array[@]}
 index=$(($RANDOM % size))
 
-echo ${array[$index]}
+#echo ${array[$index]}
 ${array[$index]}
